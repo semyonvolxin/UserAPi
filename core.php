cca00d5973b1d7e88425bc5cc605087d3be57f0c
semyon_volxin <semyon.volxin@mail.ru>
2022-06-19 04:00:45 +0300

    test commit

--- core.php
+++ core.php
@@ -1,5 +1,24 @@
-<?php 
+<?php
 namespace userApi;
+    #Подключение к бд 
+    class dataBase{
+      private static $db;
+      public function __construct($nDb){
+      	 if(($ndb instanceof Adddatabase) === true){
+      	 	static::$db = $ndb;
+      	 }
+      }
+    }
+	class Adddatabase{
+		private $host;
+		private $dbName;
+		private $user;
+		private $password;
+		public function __construct($host,$dbName,$user,$password){
+           
+		}
+	}
+
 
 
 ?>
